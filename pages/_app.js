f53e31a78115003ce5e1b69f21afcda862c62a95
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-05-29 17:17:17 +0200

    create dark layout

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@ import Navbar from "../components/Navbar";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return (
+  const getLayout = Component.getLayout || ((page) => page);
+  return getLayout(
     <>
       <Navbar />
       <Component {...pageProps} />
--- pages/about/index.js
+++ pages/about/index.js
@@ -1,15 +1,22 @@
 import React from "react";
 import Link from "next/link";
 import MainLayout from "../../layouts/MainLayout";
+import DarkLayout from "../../layouts/DarkLayout";
 
 function About() {
   return (
-    <MainLayout>
+    <>
       <h1 className={"title"}>
         Go to <Link href="/contact">Contact</Link>
       </h1>
-    </MainLayout>
+    </>
   );
 }
-
+About.getLayout = function getLayout(page) {
+  return (
+    <MainLayout>
+      <DarkLayout>{page}</DarkLayout>
+    </MainLayout>
+  );
+};
 export default About;
