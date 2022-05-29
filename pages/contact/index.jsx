93c88abc4ef98ce148316800dfcd61e0fe08be55
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-05-29 16:46:13 +0200

    feat(page): create navbars, custom links, and other pages

---
+++ pages/contact/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Link from "next/link";
+
+function Contact() {
+  return (
+    <div>
+      <h1>
+        Go to <Link href="/">Home from Contact</Link>
+      </h1>
+    </div>
+  );
+}
+
+export default Contact;
