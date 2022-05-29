e7c97fdee06f8e020867f678db39f16cc981ebe9
Jorge Jimenez <georgeguitarra07@gmail.com>
2022-05-29 16:01:48 +0200

    feat(about-page): add head title

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Head from "next/head";
 
 function About() {
   return (
     <div>
+      <Head>
+        <title>About</title>
+      </Head>
       <h1>
         Go to <Link href="/">Home</Link>
       </h1>
